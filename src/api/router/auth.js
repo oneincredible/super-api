b89e30426ddcaf9c6ba111747b3c92158a78d5cc
Pontus Persson <pontus.alexander@gmail.com>
2018-12-14 09:48:02 +0100

    Move auth layer to module.

---
+++ src/api/router/auth.js
@@ -0,0 +1,43 @@
+function createAuthorizationLayer(sessionStorage) {
+  return async function checkAuthorization(req, res, next) {
+    const auth = req.headers.authorization;
+    if (!auth) {
+      res.statusCode = 401;
+      res.send({
+        error: {
+          message: 'Authorization required.',
+        },
+      });
+      return;
+    }
+
+    const [type, token] = auth.split(' ');
+    if (type.toLowerCase() !== 'bearer') {
+      res.statusCode = 400;
+      res.send({
+        error: {
+          message: `Unknown authorization type: ${type}.`,
+        },
+      });
+      return;
+    }
+
+    const session = await sessionStorage.fetch(token);
+    if (!session) {
+      res.statusCode = 401;
+      res.send({
+        error: {
+          message: 'Invalid token.',
+        },
+      });
+      return;
+    }
+
+    req.session = session;
+    next();
+  };
+}
+
+module.exports = {
+  createAuthorizationLayer,
+};
