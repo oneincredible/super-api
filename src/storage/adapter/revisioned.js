d86cebae2e708c5484301cb547bba64d6bce8a53
Pontus Alexander <pontus.alexander@gmail.com>
2018-12-14 21:20:33 +0100

    Move query factories to adapter.

--- src/storage/adapter/revisioned.js
+++ src/storage/adapter/revisioned.js
@@ -1,11 +1,66 @@
 const { Storage } = require('../storage');
 const { createRelationStorageAdapter } = require('./relation');
-const {
-  createPromoteRevision,
-  createFetchRevision,
-  createStoreRevision,
-} = require('../query');
 const { Type } = require('../../model/field');
+const { quote } = require('../query');
+
+function createFetchRevision(Model, name) {
+  const parentTable = name;
+  const revisionTable = `${name}_revision`;
+
+  const fields = Model.fields
+    .filter(field => field.columnName)
+    .map(field => `r.${quote(field.columnName)}`);
+
+  const text = [
+    'SELECT ' + fields.join(', '),
+    `FROM ${quote(revisionTable)} r`,
+    `JOIN ${quote(parentTable)} p ON p.id = r.id AND p.revision = r.revision`,
+    'WHERE p.id = ANY($1)',
+  ].join(' ');
+
+  return function createQuery(ids) {
+    return {
+      text,
+      values: [ids],
+    };
+  };
+}
+
+function createStoreRevision(Model, name) {
+  const revisionTable = `${name}_revision`;
+
+  const fields = Model.fields.filter(field => field.columnName);
+  const columns = fields.map(field => field.columnName);
+  const placeholders = columns.map((name, index) => '$' + (index + 1));
+
+  const text = [
+    `INSERT INTO "${revisionTable}"`,
+    '(' + columns.map(quote).join(', ') + ')',
+    'VALUES (' + placeholders.join(', ') + ')',
+  ].join(' ');
+
+  return function createQuery(model) {
+    return {
+      text,
+      values: fields.map(field => field.columnValue(model)),
+    };
+  };
+}
+
+function createPromoteRevision(name) {
+  const text = [
+    `INSERT INTO "${name}" (id, revision)`,
+    `SELECT id, MAX(revision) FROM "${name}_revision" WHERE id = $1 GROUP BY id`,
+    'ON CONFLICT (id) DO UPDATE SET revision = excluded.revision',
+  ].join(' ');
+
+  return function createQuery(model) {
+    return {
+      text,
+      values: [model.id],
+    };
+  };
+}
 
 function createRevisionedStorageAdapter(Model, name) {
   const tableName = name;
