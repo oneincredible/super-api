6fcbc233c0d792ab37475ee468abcd4a47c5bd4c
Pontus Persson <pontus.alexander@gmail.com>
2018-12-14 14:17:41 +0100

    Refactor Storage test.

--- src/api/router/__tests__/storage.test.js
+++ src/api/router/__tests__/storage.test.js
@@ -19,7 +19,7 @@ describe('Storage Router', () => {
   const app = express();
   app.use('/', router);
 
-  describe('GET /', () => {
+  describe('fetching object', () => {
     describe('with malformed id', () => {
       it('responds with 400', done => {
         request(app)
@@ -70,7 +70,7 @@ describe('Storage Router', () => {
     });
   });
 
-  describe('POST /', () => {
+  describe('creating object', () => {
     let bike;
 
     beforeEach(() => {
@@ -78,7 +78,7 @@ describe('Storage Router', () => {
     });
 
     describe('with complete entity', () => {
-      beforeEach(done => {
+      it('responds with 201 and location', done => {
         request(app)
           .post('/')
           .send(bike)
@@ -86,68 +86,44 @@ describe('Storage Router', () => {
           .expect('location', `/${bike.id}`)
           .end(done);
       });
+    });
 
-      it('can be retrieved', done => {
-        request(app)
-          .get(`/${bike.id}`)
-          .expect(200)
-          .expect({
-            id: bike.id,
-            brand: 'Crescent',
-            wheelSize: 24,
-            deliveryDate: '1992-02-02T00:00:00.000Z',
-            price: { id: bike.price.id, amount: 2433.99, currency: 'USD' },
-            wheels: [],
-          })
-          .end(done);
+    describe('attaching object', () => {
+      let bike, wheels;
+
+      beforeEach(async () => {
+        bike = createBike();
+        wheels = [createWheel(), createWheel()];
+        await bikeStorage.store(bike);
+        await Promise.all(wheels.map(wheel => wheelStorage.store(wheel)));
       });
 
-      describe('then attaching wheel', () => {
-        let wheels;
-
-        beforeEach(async done => {
-          wheels = [createWheel(), createWheel()];
-
-          await Promise.all(wheels.map(wheel => wheelStorage.store(wheel)));
-
+      describe('with good ids', () => {
+        beforeEach(done => {
           request(app)
             .put(`/${bike.id}/wheels/${wheels[0].id}`)
             .expect(201)
             .end(done);
         });
 
-        it('wheel has been added to bike', done => {
+        beforeEach(done => {
+          request(app)
+            .put(`/${bike.id}/wheels/${wheels[1].id}`)
+            .expect(201)
+            .end(done);
+        });
+
+        it('wheels are available on object', done => {
           request(app)
             .get(`/${bike.id}`)
             .expect(200)
             .expect(response => {
               const bike = response.body;
-              expect(bike.wheels.length).toEqual(1);
+              expect(bike.wheels.length).toEqual(2);
               expect(bike.wheels).toContainEqual(wheels[0]);
+              expect(bike.wheels).toContainEqual(wheels[1]);
             })
             .end(done);
-        });
-
-        describe('then attaching another wheel', () => {
-          beforeEach(done => {
-            request(app)
-              .put(`/${bike.id}/wheels/${wheels[1].id}`)
-              .expect(201)
-              .end(done);
-          });
-
-          it('both wheels exists on bike', done => {
-            request(app)
-              .get(`/${bike.id}`)
-              .expect(200)
-              .expect(response => {
-                const bike = response.body;
-                expect(bike.wheels.length).toEqual(2);
-                expect(bike.wheels).toContainEqual(wheels[0]);
-                expect(bike.wheels).toContainEqual(wheels[1]);
-              })
-              .end(done);
-          });
         });
 
         describe('then deleting wheel', () => {
@@ -158,39 +134,16 @@ describe('Storage Router', () => {
               .end(done);
           });
 
-          it('there is no wheels on bike', done => {
+          it('wheel is deleted from bike', done => {
             request(app)
               .get(`/${bike.id}`)
               .expect(200)
               .expect(response => {
                 const bike = response.body;
-                expect(bike.wheels.length).toEqual(0);
+                expect(bike.wheels.length).toEqual(1);
               })
               .end(done);
           });
-        });
-      });
-
-      describe('then with existing id', () => {
-        beforeEach(done => {
-          bike.brand = 'DSB';
-
-          request(app)
-            .post('/')
-            .send(bike)
-            .expect(201)
-            .expect('location', `/${bike.id}`)
-            .end(done);
-        });
-
-        it('has been updated', done => {
-          request(app)
-            .get(`/${bike.id}`)
-            .expect(200)
-            .expect(response => {
-              expect(response.body.brand).toEqual('DSB');
-            })
-            .end(done);
         });
       });
     });
